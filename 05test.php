6a26511f4e8a5e99fae89d5bd7ca5a46374828f8
takano <goyataiko@gmail.com>
2023-07-06 06:16:02 +0000

    05php作成

---
+++ 05test.php
@@ -0,0 +1,34 @@
+<?php
+
+$name= "takano";
+
+if ( $name == "takano") {
+    echo "私は あなたの名前 です
+";} else {"あなたの名前ではありません
+";}
+
+$a = 0;
+for ($i=1;$i<=10000;$i++){
+    $a += $i;
+}
+echo "$a
+";
+
+
+$fruits = array("1apple", "2orange", "3lemon", "4emon", "5emon");
+foreach( $fruits as $fruit) {
+    echo "$fruit \n";
+}
+
+
+
+$start = 1;
+$end = 100;
+
+for($i = $start; $i < $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo "$i ";
+  }
+} echo "\n";
