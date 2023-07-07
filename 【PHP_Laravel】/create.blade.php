89f947dc96312ce2f3baf94954110c04f6b4408f
takano <goyataiko@gmail.com>
2023-07-07 09:05:39 +0000

    php05問題、フォルダ作成、ファイル移動

---
+++ 【PHP_Laravel】/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>profile</title>
+    </head>
+    <body>
+        <!--場所:mynews/resources/views/admin/profile-->
+        <h1>profile作成画面</h1>
+        <h2>add 機能</h2>
+        <h2>create 機能</h2>
+    </body>
+</html>
